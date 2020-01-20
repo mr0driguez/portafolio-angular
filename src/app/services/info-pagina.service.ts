9b3f5da3d03cb85610fbd0220684bb568fc70f53
Rodriguez Morales <mrodriguez1050@gmail.com>
2020-01-20 23:35:41 +0100

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,17 +10,34 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any [] = [];
+
   constructor( private http: HttpClient ) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
-    // Leer el archivo JSON
-    this.http.get('assets/data/data-pagina.json')
+  private cargarInfo() {
+        // Leer el archivo JSON
+        this.http.get('assets/data/data-pagina.json')
+        // tslint:disable-next-line: deprecation
+        .subscribe( (resp: InfoPagina) => {
+  
+          this.cargada = true;
+          this.info = resp;
+        });
+  }
+
+  private cargarEquipo() {
+
+      // Leer el archivo JSON
+      this.http.get('https://angular-html-d3176.firebaseio.com/equipo.json')
       // tslint:disable-next-line: deprecation
-      .subscribe( (resp: InfoPagina) => {
-
-        this.cargada = true;
-        this.info = resp;
-        console.log(resp);
+      .subscribe( ( resp: any[]) => {
+        this.equipo = resp;
+        //console.log(resp);
       });
 
+      //this.equipo = resp
   }
 }
